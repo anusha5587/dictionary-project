c2ca8b349b199a83cf98b6ed9f5e229979a1c0ab
anusha5587 <anoosha.anand.24@gmail.com>
2024-02-01 15:20:19 +0000

    made some changes to CSS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -18,30 +18,16 @@ export default function Dictionary() {
       Dictionary
       <div className="searchBar">
         <form onSubmit={search}>
-          <div className="row">
-            <div className="col-8">
-              <input
-                type="search"
-                placeholder="Search here...."
-                className="form-control"
-                aria-label="Search"
-                aria-describedby="addon-wrapping"
-                autocomplete="off"
-                autoFocus={true}
-                onChange={handleKeywordChange}
-              />
-            </div>
-            <div className="col-4 d-flex justify-content-between">
-              <input
-                type="submit"
-                value=" Search "
-                placeholder="Search"
-                className="btn btn-primary"
-                style={{ border: "none" }}
-                id="search-icon"
-              />
-            </div>
-          </div>
+          <input
+            type="search"
+            placeholder="Search here...."
+            className="form-control"
+            aria-label="Search"
+            aria-describedby="addon-wrapping"
+            autocomplete="off"
+            autoFocus={true}
+            onChange={handleKeywordChange}
+          />
         </form>
       </div>
     </div>
